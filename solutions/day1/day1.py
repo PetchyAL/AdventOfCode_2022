fd64e697f17b57b358cdf5c5735097bdff83fd03
Lewis Petch <90348839+PetchyAL@users.noreply.github.com>
2022-12-04 19:31:34 +0000

    + Refactored to keep map as iterable
    map was being needlessly cast to a list, removed unnecessary casting

--- solutions/day1/day1.py
+++ solutions/day1/day1.py
@@ -1,10 +1,10 @@
-text_file = open('input.txt')
-calorie_list = text_file.read().split('\n\n')
-totals = []
+with open('input.txt') as text_file:
+  calorie_list = text_file.read().split('\n\n')
+  totals = []
 
-for cals in calorie_list:
-  totals.append(sum(list(map(int,cals.strip().split('\n')))))
-totals.sort()
+  for cals in calorie_list:
+    totals.append(sum(map(int,cals.strip().split('\n'))))
+  totals.sort()
 
 print(totals[-1]) #Part 1 result
 print(sum(totals[-3:])) #Part 2 result
