d7ad4e3ade27fee45e713685c578333757acb8bc
Lewis Petch <90348839+PetchyAL@users.noreply.github.com>
2022-12-04 19:41:06 +0000

    Edited spacing for consistency

--- solutions/day4/day4.py
+++ solutions/day4/day4.py
@@ -3,14 +3,14 @@ def prep(line):
   a, b = a.split('-'), b.split('-')
   a = list(range(int(a[0]), int(a[1])+1))
   b = list(range(int(b[0]), int(b[1])+1))
-  return a,b
+  return a, b
 
 def contains(line):
-  a,b = prep(line)
+  a, b = prep(line)
   return 1 if all(num in a for num in b) or all(num in b for num in a) else 0
 
 def overlaps(line):
-  a,b = prep(line)
+  a, b = prep(line)
   return 1 if any(num in a for num in b) else 0
 
 with open("input.txt") as text_file:
