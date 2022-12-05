d04ff273f84ad5e7dec05020114a113fce3663b0
Lewis Petch <90348839+PetchyAL@users.noreply.github.com>
2022-12-05 15:11:03 +0000

    Refactored loops for efficiency
    Rather than doing each part in its own for loop, they are performed within the same "for instruction in instructions.split('\n')" loop

--- solutions/day5/day5.py
+++ solutions/day5/day5.py
@@ -12,21 +12,15 @@ def prep_cargo(cargo):
 with open("input.txt") as text_file:
   cargo, instructions = text_file.read().strip().split('\n\n')
 cargo = prep_cargo(cargo.split('\n'))
-
-#Part 1
 cargo_one = copy.deepcopy(cargo)
-for instruction in instructions.split('\n'):
-  i = instruction.strip().split(' ')
-  n, fr, to = int(i[1]), int(i[3]), int(i[5])
-  for _ in range(n):
-    cargo_one[to-1].insert(0, cargo_one[fr-1].pop(0))
-
-#Part 2
 cargo_two = copy.deepcopy(cargo)
+
 for instruction in instructions.split('\n'):
   i = instruction.strip().split(' ')
   n, fr, to = int(i[1]), int(i[3]), int(i[5])
-  for num in reversed(range(n)):
+  for _ in range(n): #Part 1
+    cargo_one[to-1].insert(0, cargo_one[fr-1].pop(0))
+  for num in reversed(range(n)): #Part 2
     cargo_two[to-1].insert(0, cargo_two[fr-1].pop(num))
 
 print(''.join(list(stack[0] for stack in cargo_one))) #Part 1 result
