d621ca95f8d72cb81a4ba931fc2e8ce4a3a9744f
Lewis Petch <90348839+PetchyAL@users.noreply.github.com>
2022-12-06 21:23:09 +0000

    Refactored to remove unnecessary for loop
    For loop ensured that set only contained duplicate characters, I refactored the code to instead compare against the marker length. Reduced execution time by almost 50%

--- solutions/day6/day6.py
+++ solutions/day6/day6.py
@@ -1,7 +1,7 @@
 def marker_finder(coms, marker_len):
   for n in range(marker_len, len(coms)):
     marker_set = coms[n-marker_len:n]
-    if len(set(c for c in marker_set if marker_set.count(c)>1)) == 0:
+    if len(set(marker_set)) == marker_len:
       return(n)
 
 with open("input.txt") as text_file:
