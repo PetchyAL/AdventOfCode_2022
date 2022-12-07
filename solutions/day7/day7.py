14c1348b9537800520b022785e1c6e35b2812758
Lewis Petch <90348839+PetchyAL@users.noreply.github.com>
2022-12-07 17:04:39 +0000

    Refactored for efficiency and readability

--- solutions/day7/day7.py
+++ solutions/day7/day7.py
@@ -2,47 +2,36 @@ from string import digits
 with open("input.txt") as text_file:
   data = text_file.read().strip()
 
-dir_sizes = dict()
-checked_f = dict()
-crnt_dir = []
+dir_sizes = dict() #Keeps a size value for each directory
+checked_f = dict() #Which directories and files were already checked
+crnt_dir = [] #Current directory path/structure
 
 for line in data.split('\n'):
-  ln = line.strip()
+  ln = line.strip().split(' ')
   #Changing current directory
-  if ln.startswith('$ cd'):
-    if len(crnt_dir) == 0:
-      crnt_dir.append('/')
-    elif ln.split(' ')[2] == '/':
-      crnt_dir = crnt_dir[0]
-    elif ln.split(' ')[2] == '..':
-      crnt_dir = crnt_dir[:-1]
-    else: crnt_dir.append(ln.split(' ')[2])
+  if ln[1] == 'cd':
+    if ln[2] == '/':
+      try: crnt_dir = [crnt_dir[0]]
+      except: crnt_dir.append(ln[2])
+    elif ln[2] == '..':
+      crnt_dir.pop()
+    else: crnt_dir.append(ln[2])
 
   #Get file sizes
-  elif ln[0] in digits:
+  elif ln[0][0] in digits:
     dir_str = '\\'.join(crnt_dir)
-    if dir_str not in checked_f.keys():
-      checked_f[dir_str] = []
-    if ln.split(' ')[1] not in checked_f[dir_str]:
-      checked_f[dir_str].append(ln.split(' ')[1])
+    if dir_str not in checked_f: checked_f[dir_str] = []
+    #Check if file size was already added
+    if ln[1] not in checked_f[dir_str]:
+      checked_f[dir_str].append(ln[1])
       for f in range(len(crnt_dir)+1):
         if '\\'.join(crnt_dir[:f]) not in dir_sizes.keys():
-          if '\\'.join(crnt_dir[:f]).strip() == '':
-            continue
           dir_sizes['\\'.join(crnt_dir[:f])] = 0
-        dir_sizes['\\'.join(crnt_dir[:f])] += int(line.split(' ')[0])
-
+        dir_sizes['\\'.join(crnt_dir[:f])] += int(ln[0])
+    
 #Part 1
-sum = 0
-for n in dir_sizes.values():
-  if n < 100000:
-    sum += n
-print(sum)
+print(sum(x for x in dir_sizes.values() if x <= 100000))
 
 #Part 2
-sort_vals = sorted(dir_sizes.values())
-space_needed = 30000000-(70000000-sort_vals[-1])
-for n in sort_vals:
-  if n >= space_needed:
-    print(n)
-    break
+space_needed = 30000000-(70000000-max(dir_sizes.values()))
+print(min(x for x in dir_sizes.values() if x >= space_needed))
